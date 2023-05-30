61aecb645c9aea8593d3d30cc50748ffc051e871
Max Bernstein <max@bernsteinbear.com>
2023-05-30 11:23:15 -0400

    Cache globals and type attributes

--- superoptimizer.py
+++ superoptimizer.py
@@ -25,15 +25,19 @@ class Superoptimizer:
 
     # Generates all possible programs.
     def generate_programs(self, cpu, max_length, max_mem, max_val):
+        load = CPU.load
+        swap = CPU.swap
+        xor = CPU.xor
+        inc = CPU.inc
         for length in range(1, max_length + 1):
             for prog in product(cpu.ops.values(), repeat=length):
                 arg_sets = []
                 for op in prog:
-                    if op == CPU.load:
+                    if op == load:
                         arg_sets.append([tuple([val]) for val in range(max_val + 1)])
-                    elif op == CPU.swap or op == CPU.xor:
+                    elif op == swap or op == xor:
                         arg_sets.append(product(range(max_mem), repeat=2))
-                    elif op == CPU.inc:
+                    elif op == inc:
                         arg_sets.append([tuple([val]) for val in range(max_mem)])
                 for arg_set in product(*arg_sets):
                     program = [(op, *args) for op, args in zip(prog, arg_set)] 
