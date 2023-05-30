0f07b88e1f4af8ce9a2d3deaf2d4bfe6545db197
Max Bernstein <max@bernsteinbear.com>
2023-05-30 12:00:47 -0400

    Make state less functional
    We modify the internal values, not the reference. Don't re-assign.

--- cpu.py
+++ cpu.py
@@ -8,23 +8,19 @@ class CPU:
         for instruction in program:
             op = instruction[0]
             args = instruction[1]
-            state = op(self, state, *args)
+            op(self, state, *args)
         return state
 
     def load(self, state, val):
         state[0] = val
-        return state
     
     def swap(self, state, mem1, mem2):
         state[mem1], state[mem2] = state[mem2], state[mem1]
-        return state
     
     def xor(self, state, mem1, mem2):
         state[mem1] = state[mem1] ^ state[mem2]
-        return state
     
     def inc(self, state, mem):
         state[mem] += 1
-        return state
 
     ops = {'LOAD': load, 'SWAP': swap, 'XOR': xor, 'INC': inc}
